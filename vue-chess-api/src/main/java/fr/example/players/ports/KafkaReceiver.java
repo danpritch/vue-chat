f7ee64c9a2057fd22405b3d69d8ece2326615ce5
Dan Pritchard <danmarkpritch@gmmail.com>
2025-03-08 16:50:23 +0000

    Kafka topic now emits to websocket.

--- vue-chess-api/src/main/java/fr/example/players/ports/KafkaReceiver.java
+++ vue-chess-api/src/main/java/fr/example/players/ports/KafkaReceiver.java
@@ -6,15 +6,21 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import fr.example.players.Player;
+import fr.example.players.PlayerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class KafkaReceiver {
+	
+	private final PlayerService playerService;
 
 	@KafkaListener(topics = "postgres.vue_chess.players", containerFactory = "playerKafkaListenerContainerFactory")
 	public void listen(@Header(KafkaHeaders.RECEIVED_KEY) String key, Player player) {
 		log.info("Received message with key: " + key + " and value: " + player.toString());
+		playerService.emitPlayer(player);
 	}
 
 }
